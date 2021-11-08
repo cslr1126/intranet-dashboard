bcdfab761f6062921ce279f6bb125f40234684c7
Cliff Sanchez <clifford.sanchez@liferay.com>
2021-11-08 09:59:54 -0500

    Added Web Component API

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# intranet-dashboard
-Modern Intranet dashboard
+## intranet-dashboard
+Modern Intranet dashboard is a simple React app that enables users to view personalized content and have a robust profile area.
+
+### Remote app enabled
+Leverages browser Web Component API, which enables Remote App integration.
+
+## install
+node install
+
+## run
+yarn start
+
--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+class WebComponent extends HTMLElement {
+  connectedCallback() {
+          ReactDOM.render(
+                  <App />,
+                  this
+          );
+  }
+}
+
+const ELEMENT_ID = 'intranet-dashboard';
+
+if (!customElements.get(ELEMENT_ID)) {
+  customElements.define(ELEMENT_ID, WebComponent);
+}
